42b6645586dcc8d39ebb2b2ed766abd6c06057dc
bluelovers <codelovers@users.sourceforge.net>
2017-08-26 02:34:09 +0800

    reformat code style for vcs (Step: 1)
    Step: 1
    
    [ M] modified - Changes not staged for commit:
    
    	src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,87 +1,112 @@
-interface ITaskObject {
-  aliases?: string[];
-  description?: string;
-  options?: ITaskOptions;
-  tasks?: string[];
+interface ITaskObject
+{
+	aliases?: string[];
+	description?: string;
+	options?: ITaskOptions;
+	tasks?: string[];
 
-  deps?: Array<string>;
-  callback?: Function;
-}
-interface ITaskOptions {
-  options?: any;
-  aliases?: string[];
+	deps?: Array<string>;
+	callback?: Function;
 }
 
-function looksLikeGulp(gulp): boolean {
-  return !!(gulp.task && gulp.watch && gulp.src && gulp.dest);
+interface ITaskOptions
+{
+	options?: any;
+	aliases?: string[];
 }
 
-function looksLikeGulpHelp(gulp): boolean {
-  return !!(gulp.tasks.help);
+function looksLikeGulp(gulp): boolean
+{
+	return !!(gulp.task && gulp.watch && gulp.src && gulp.dest);
 }
 
-function getOptions(impl): ITaskOptions {
-  const options = impl.options || {};
-  if ( options.options || options.aliases ) {
-    return options;
-  }
-  return {
-    options: impl.options || {},
-    aliases: impl.aliases || []
-  };
+function looksLikeGulpHelp(gulp): boolean
+{
+	return !!(gulp.tasks.help);
 }
 
-function isTaskListObject(impl: any = {}): boolean {
-  return Object.keys(impl).indexOf('tasks') > -1;
+function getOptions(impl): ITaskOptions
+{
+	const options = impl.options || {};
+	if (options.options || options.aliases)
+	{
+		return options;
+	}
+	return {
+		options: impl.options || {},
+		aliases: impl.aliases || []
+	};
 }
 
-function getTasks(tasks) {
-  if ( typeof tasks === 'function' ) {
-    tasks = tasks();
-  }
-  return tasks || [];
+function isTaskListObject(impl: any = {}): boolean
+{
+	return Object.keys(impl).indexOf('tasks') > -1;
 }
 
-function prefixTasks(tasks: Array<string> = [], parentTask: string = '') : Array<string> {
-  return (tasks || []).map(function (v: string) {
-    return v.indexOf(':') === 0 ? parentTask + v : v;
-  });
+function getTasks(tasks)
+{
+	if (typeof tasks === 'function')
+	{
+		tasks = tasks();
+	}
+	return tasks || [];
 }
 
-function addTasksToGulp(gulp, rs, taskObject: ITaskObject, parentTask: string = ''): void {
-  for ( let taskName in taskObject ) {
-    const taskImpl = taskObject[taskName];
-    if ( parentTask ) {
-      taskName = `${parentTask}:${taskName}`;
-    }
-    if (Object.keys(taskImpl).indexOf('callback') > -1) {
-      const taskFn = taskImpl.callback;
-      gulp.task(taskName, taskImpl.description, prefixTasks(taskImpl.deps, parentTask), taskFn, getOptions(taskImpl));
-    } else if ( typeof taskImpl === 'function' || isTaskListObject(taskImpl) ) {
-      const taskFn = typeof taskImpl === 'function' ? taskImpl : function(done) {
-        return rs.use(gulp)(...(getTasks(taskImpl.tasks)).concat(done));
-      };
-      gulp.task(taskName, taskImpl.description, prefixTasks(taskImpl.deps, parentTask), taskFn, getOptions(taskImpl));
-    } else {
-      addTasksToGulp(gulp, rs, taskImpl, taskName);
-    }
-  }
+function prefixTasks(tasks: Array<string> = [], parentTask: string = ''): Array<string>
+{
+	return (tasks || []).map(function (v: string)
+	{
+		return v.indexOf(':') === 0 ? parentTask + v : v;
+	});
 }
 
-export = function gulpAddTasks(gulpInstance): any {
-  if ( !looksLikeGulp(gulpInstance) ) {
-    throw new Error('An instance of gulp is required as the first argument');
-  }
+function addTasksToGulp(gulp, rs, taskObject: ITaskObject, parentTask: string = ''): void
+{
+	for (let taskName in taskObject)
+	{
+		const taskImpl = taskObject[taskName];
+		if (parentTask)
+		{
+			taskName = `${parentTask}:${taskName}`;
+		}
+		if (Object.keys(taskImpl).indexOf('callback') > -1)
+		{
+			const taskFn = taskImpl.callback;
+			gulp.task(taskName, taskImpl.description, prefixTasks(taskImpl.deps, parentTask), taskFn, getOptions(taskImpl));
+		}
+		else if (typeof taskImpl === 'function' || isTaskListObject(taskImpl))
+		{
+			const taskFn = typeof taskImpl === 'function' ? taskImpl : function (done)
+			{
+				return rs.use(gulp)(...(getTasks(taskImpl.tasks)).concat(done));
+			};
+			gulp.task(taskName, taskImpl.description, prefixTasks(taskImpl.deps, parentTask), taskFn, getOptions(taskImpl));
+		}
+		else
+		{
+			addTasksToGulp(gulp, rs, taskImpl, taskName);
+		}
+	}
+}
 
-  return function(...taskObjects) {
-    const runSequence = require('run-sequence');
-    const gulp = looksLikeGulpHelp(gulpInstance) ? gulpInstance : require('gulp-help')(gulpInstance);
+export = function gulpAddTasks(gulpInstance): any
+{
+	if (!looksLikeGulp(gulpInstance))
+	{
+		throw new Error('An instance of gulp is required as the first argument');
+	}
 
-    taskObjects
-      .forEach((taskObject: ITaskOptions) => {
-        addTasksToGulp(gulp, runSequence, taskObject);
-      });
+	return function (...taskObjects)
+	{
+		const runSequence = require('run-sequence');
+		const gulp = looksLikeGulpHelp(gulpInstance) ? gulpInstance : require('gulp-help')(gulpInstance);
 
-    return gulp;
-  };
+		taskObjects
+			.forEach((taskObject: ITaskOptions) =>
+			{
+				addTasksToGulp(gulp, runSequence, taskObject);
+			});
+
+		return gulp;
+	};
 }
